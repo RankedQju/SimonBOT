09ba12b47f5f4e5db87c2831d700cfff47118c50
SimonB <62061153+RankedQju@users.noreply.github.com>
2020-03-11 15:41:38 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 client.login(process.env.BOT_TOKEN);
 
-var request = require('request');
+var mcping = require('mc-ping');
 var mcCommand = 'mcstat'; // Command for triggering
 var mcIP = '158.69.137.37'; // Your MC server IP or hostname address
 var mcPort = 25565; // Your MC server port (25565 is the default)
@@ -20,8 +20,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === mcCommand) {
-        var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
-        request(url, function(err, response, body) {
+        mcping(mcIP, mcPort, function(err, res) {
             if(err) {
                 console.log(err);
                 return message.reply('Error getting Minecraft server status...');
@@ -37,6 +36,6 @@ client.on('message', message => {
                 }
             }
             message.reply(status);
-        });
+        }, 3000);
     }
 });
