ab11debd3fc2d87cf4df47f5ac7558b46c12b839
SimonB <62061153+RankedQju@users.noreply.github.com>
2020-03-11 14:04:36 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,8 +1,13 @@
 const Discord = require('discord.js');
-
+const Request = require('request.js');
 const client = new Discord.Client();
 
+client.login('Njg3MjcyMzU0MTY1OTQ4NDIw.XmjWig.6gzkwg7dntEX1PztQreZySzfjts');
 
+var request = require('request');
+var mcCommand = '/minecraft'; // Command for triggering
+var mcIP = '158.69.137.37'; // Your MC server IP or hostname address
+var mcPort = 25565; // Your MC server port (25565 is the default)
 
 client.on('ready', () => {
 
@@ -11,15 +16,27 @@ client.on('ready', () => {
 });
 
 
-
 client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
+    if (message.content === mcCommand) {
+        var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
+        request(url, function(err, response, body) {
+            if(err) {
+                console.log(err);
+                return message.reply('Error getting Minecraft server status...');
+            }
+            body = JSON.parse(body);
+            var status = '*Minecraft server is currently offline*';
+            if(body.online) {
+                status = '**Minecraft** server is **online**  -  ';
+                if(body.players.now) {
+                    status += '**' + body.players.now + '** people are playing!';
+                } else {
+                    status += '*Nobody is playing!*';
+                }
+            }
+            message.reply(status);
+        });
+    }
 });
 
 
